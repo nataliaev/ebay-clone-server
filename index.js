b515b0289e5cd445579e5049655650fc95410db2
nataliaev <nataliiaevchenko@gmail.com>
2019-08-07 14:34:12 +0200

    Add process.env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const corsMiddleware = cors();
 app.use(corsMiddleware);
